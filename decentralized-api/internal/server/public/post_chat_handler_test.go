aa591c99e5240e851445ce8e9cfed81b10857677
John Long <patimen@yahoo.com>
2026-03-05 23:30:25 -0800

    Fix unit tsts

--- decentralized-api/internal/server/public/post_chat_handler_test.go
+++ decentralized-api/internal/server/public/post_chat_handler_test.go
@@ -6,7 +6,6 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
-	"strconv"
 	"testing"
 
 	"decentralized-api/chainphase"
@@ -14,8 +13,6 @@ import (
 
 	"github.com/productscience/inference/x/inference/types"
 	"github.com/stretchr/testify/require"
-
-	"decentralized-api/completionapi"
 )
 
 // createTestRequest creates a test HTTP request with the given body
